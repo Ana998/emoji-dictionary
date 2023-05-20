e2f29df0d50aef908765d2b6177225036e342308
Ana998 <48926623+Ana998@users.noreply.github.com>
2023-05-21 00:50:06 +0200

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🥰",
+    name: "Smiling face with smiling eyes and three hearts",
+    meaning:
+      "This adorable emoji represents a smiling face surrounded by hearts, embodying infatuation and romance. It's perfect for expressing affection, love and romantic affairs of the heart while floating on cloud nine."
+  },
+  {
+    id: 5,
+    emoji: "😭",
+    name: "Loudly Crying Face",
+    meaning:
+      "A face crying violently symbolizes despair and inconsolability, while the tears flow from the eyes like a waterfall. It represents feelings of sadness, pain, or defeat, and is sometimes used in ironic contexts."
+  },
+  {
+    id: 6,
+    emoji: "🤓",
+    name: "Nerd Face",
+    meaning:
+      "Nerd emoji is a face with big glasses, an awkward smile and buck teeth. It's often used in a humorous or ironic way to describe nerds or to express how smart you are. "
+  }
+];
+
+export default emojipedia;
